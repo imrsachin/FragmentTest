b35d337389436c2bf2131b4d32d257dbbded463a
Sachin <imrsachinrwt@gmail.com>
2021-01-07 09:10:34 +0530

    nav host fragment added

---
+++ app/src/main/java/com/onlinehowtodo/fragmenttest/Product.kt
@@ -0,0 +1,4 @@
+package com.onlinehowtodo.fragmenttest
+
+data class Product(val id: Int, val name: String, val price: Float, val imageId: Int, val shortDescription: String,
+                   val longDescription: String)
